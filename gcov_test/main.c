dba6d4ddad50c32841d94dd69a645ff84faf041d
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-03-05 11:07:49 +0100

    gcov_test

---
+++ gcov_test/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void hello(const char* format, int n, ...) {
+	va_list argp;
+	va_start(argp, n);
+
+	for (int i = 0; i < n; i++) {
+		int val = va_arg(argp, int);
+		printf(format, val);
+	}
+	va_end(argp);
+}
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		printf("Hello World %d!!!\n", i);
+	}
+
+	hello("Hello %d\n" , 4, 1, 2, 4, 5);
+}
