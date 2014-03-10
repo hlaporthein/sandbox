330073aab886f9a70edb6670e017d5eaac3d49ed
Jean-Louis GUENEGO <jlguenego@gmail.com>
2014-03-10 16:26:45 +0100

    ok

--- png_test/main.c
+++ png_test/main.c
@@ -1,189 +1,161 @@
-/*
- * Copyright 2002-2011 Guillaume Cottenceau and contributors.
- *
- * This software may be freely redistributed under the terms
- * of the X11 license.
- *
- */
-
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
-
-#define PNG_DEBUG 3
 #include <png.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-void abort_(const char * s, ...)
+/* A coloured pixel. */
+
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} pixel_t;
+
+/* A picture. */
+
+typedef struct  {
+    pixel_t *pixels;
+    size_t width;
+    size_t height;
+} bitmap_t;
+
+/* Given "bitmap", this returns the pixel of bitmap at the point
+   ("x", "y"). */
+
+static pixel_t * pixel_at (bitmap_t * bitmap, int x, int y)
 {
-        va_list args;
-        va_start(args, s);
-        vfprintf(stderr, s, args);
-        fprintf(stderr, "\n");
-        va_end(args);
-        abort();
+    return bitmap->pixels + bitmap->width * y + x;
 }
 
-int x, y;
+/* Write "bitmap" to a PNG file specified by "path"; returns 0 on
+   success, non-zero on error. */
 
-int width, height, rowbytes;
-png_byte color_type;
-png_byte bit_depth;
-
-png_structp png_ptr;
-png_infop info_ptr;
-int number_of_passes;
-png_bytep * row_pointers;
-
-void read_png_file(char* file_name)
+static int save_png_to_file (bitmap_t *bitmap, const char *path)
 {
-        unsigned char header[8];    // 8 is the maximum size that can be checked
+    FILE * fp;
+    png_structp png_ptr = NULL;
+    png_infop info_ptr = NULL;
+    size_t x, y;
+    png_byte ** row_pointers = NULL;
+    /* "status" contains the return value of this function. At first
+       it is set to a value which means 'failure'. When the routine
+       has finished its work, it is set to a value which means
+       'success'. */
+    int status = -1;
+    /* The following number is set by trial and error only. I cannot
+       see where it it is documented in the libpng manual.
+    */
+    int pixel_size = 3;
+    int depth = 8;
 
-        /* open file and test for it being a png */
-        FILE *fp = fopen(file_name, "rb");
-        if (!fp)
-                abort_("[read_png_file] File %s could not be opened for reading", file_name);
-        fread(header, 1, 8, fp);
-        if (png_sig_cmp(header, 0, 8))
-                abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+    fp = fopen (path, "wb");
+    if (! fp) {
+        goto fopen_failed;
+    }
 
+    png_ptr = png_create_write_struct (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (png_ptr == NULL) {
+        goto png_create_write_struct_failed;
+    }
 
-        /* initialize stuff */
-        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    info_ptr = png_create_info_struct (png_ptr);
+    if (info_ptr == NULL) {
+        goto png_create_info_struct_failed;
+    }
 
-        if (!png_ptr)
-                abort_("[read_png_file] png_create_read_struct failed");
+    /* Set up error handling. */
 
-        info_ptr = png_create_info_struct(png_ptr);
-        if (!info_ptr)
-                abort_("[read_png_file] png_create_info_struct failed");
+    if (setjmp (png_jmpbuf (png_ptr))) {
+        goto png_failure;
+    }
 
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[read_png_file] Error during init_io");
+    /* Set image attributes. */
 
-        png_init_io(png_ptr, fp);
-        png_set_sig_bytes(png_ptr, 8);
+    png_set_IHDR (png_ptr,
+                  info_ptr,
+                  bitmap->width,
+                  bitmap->height,
+                  depth,
+                  PNG_COLOR_TYPE_RGB,
+                  PNG_INTERLACE_NONE,
+                  PNG_COMPRESSION_TYPE_DEFAULT,
+                  PNG_FILTER_TYPE_DEFAULT);
 
-        png_read_info(png_ptr, info_ptr);
+    /* Initialize rows of PNG. */
 
-        width = png_get_image_width(png_ptr, info_ptr);
-        height = png_get_image_height(png_ptr, info_ptr);
-        color_type = png_get_color_type(png_ptr, info_ptr);
-        bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-        number_of_passes = png_set_interlace_handling(png_ptr);
-        png_read_update_info(png_ptr, info_ptr);
-
-
-        /* read file */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[read_png_file] Error during read_image");
-
-        row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-
-        if (bit_depth == 16)
-                rowbytes = width*8;
-        else
-                rowbytes = width*4;
-
-        for (y=0; y<height; y++)
-                row_pointers[y] = (png_byte*) malloc(rowbytes);
-
-        png_read_image(png_ptr, row_pointers);
-
-        fclose(fp);
-}
-
-
-void write_png_file(char* file_name)
-{
-        /* create file */
-        FILE *fp = fopen(file_name, "wb");
-        if (!fp)
-                abort_("[write_png_file] File %s could not be opened for writing", file_name);
-
-
-        /* initialize stuff */
-        png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
-        if (!png_ptr)
-                abort_("[write_png_file] png_create_write_struct failed");
-
-        info_ptr = png_create_info_struct(png_ptr);
-        if (!info_ptr)
-                abort_("[write_png_file] png_create_info_struct failed");
-
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during init_io");
-
-        png_init_io(png_ptr, fp);
-
-
-        /* write header */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during writing header");
-
-        png_set_IHDR(png_ptr, info_ptr, width, height,
-                     8, 6, PNG_INTERLACE_NONE,
-                     PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-        png_write_info(png_ptr, info_ptr);
-
-
-        /* write bytes */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during writing bytes");
-
-        png_write_image(png_ptr, row_pointers);
-
-
-        /* end write */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[write_png_file] Error during end of write");
-
-        png_write_end(png_ptr, NULL);
-
-        /* cleanup heap allocation */
-        for (y=0; y<height; y++)
-                free(row_pointers[y]);
-        free(row_pointers);
-
-        fclose(fp);
-}
-
-
-void process_file(void)
-{
-        /* Expand any grayscale, RGB, or palette images to RGBA */
-        png_set_expand(png_ptr);
-
-        /* Reduce any 16-bits-per-sample images to 8-bits-per-sample */
-        png_set_strip_16(png_ptr);
-
-        for (y=0; y<height; y++) {
-                png_byte* row = row_pointers[y];
-                for (x=0; x<width; x++) {
-                        png_byte* ptr = &(row[x*4]);
-                        printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
-                               x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
-
-                        /* perform whatever modifications needed, for example to set red value to 0 and green value to the blue one:
-                           ptr[0] = 0;
-                           ptr[1] = ptr[2]; */
-                }
+    row_pointers = png_malloc (png_ptr, bitmap->height * sizeof (png_byte *));
+    for (y = 0; y < bitmap->height; ++y) {
+        png_byte *row =
+            png_malloc (png_ptr, sizeof (uint8_t) * bitmap->width * pixel_size);
+        row_pointers[y] = row;
+        for (x = 0; x < bitmap->width; ++x) {
+            pixel_t * pixel = pixel_at (bitmap, x, y);
+            *row++ = pixel->red;
+            *row++ = pixel->green;
+            *row++ = pixel->blue;
         }
+    }
+
+    /* Write the image data to "fp". */
+
+    png_init_io (png_ptr, fp);
+    png_set_rows (png_ptr, info_ptr, row_pointers);
+    png_write_png (png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    /* The routine has successfully written the file, so we set
+       "status" to a value which indicates success. */
+
+    status = 0;
+
+    for (y = 0; y < bitmap->height; y++) {
+        png_free (png_ptr, row_pointers[y]);
+    }
+    png_free (png_ptr, row_pointers);
+
+ png_failure:
+ png_create_info_struct_failed:
+    png_destroy_write_struct (&png_ptr, &info_ptr);
+ png_create_write_struct_failed:
+    fclose (fp);
+ fopen_failed:
+    return status;
 }
 
+/* Given "value" and "max", the maximum value which we expect "value"
+   to take, this returns an integer between 0 and 255 proportional to
+   "value" divided by "max". */
 
-int main(int argc, char **argv)
+static int pix (int value, int max)
 {
-        if (argc != 3)
-                abort_("Usage: program_name <file_in> <file_out>");
-
-        read_png_file(argv[1]);
-        process_file();
-        write_png_file(argv[2]);
-
+    if (value < 0)
         return 0;
+    return (int) (256.0 *((double) (value)/(double) max));
+}
+
+int main ()
+{
+    bitmap_t fruit;
+    int x;
+    int y;
+
+    /* Create an image. */
+
+    fruit.width = 100;
+    fruit.height = 100;
+
+    fruit.pixels = calloc (sizeof (pixel_t), fruit.width * fruit.height);
+
+    for (y = 0; y < fruit.height; y++) {
+        for (x = 0; x < fruit.width; x++) {
+            pixel_t * pixel = pixel_at (& fruit, x, y);
+            pixel->red = pix (x, fruit.width);
+            pixel->green = pix (y, fruit.height);
+        }
+    }
+
+    /* Write the image to a file 'fruit.png'. */
+
+    save_png_to_file (& fruit, "fruit.png");
+
+    return 0;
 }
