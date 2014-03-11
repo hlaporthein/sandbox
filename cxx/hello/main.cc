9f6447983248d3bb840967e7c26550e510d319b9
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-03-11 17:09:49 +0100

    hello

--- cxx/hello/main.cc
+++ cxx/hello/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #define NL cout << endl
 
 using namespace std;
@@ -7,18 +8,72 @@ extern string yannis;
 
 class Yannis {
 	public:
-		void operator<<(string str) {
+		Yannis operator<<(string str) {
 			cout << "Yannis: " << str << endl;
+			return *this;
+		}
+		Yannis operator<<(int a) {
+			cout << "Yannis: " << a << endl;
+			return *this;
+		}
+
+//		void operator and(string str) {
+//			cout << "Yannis: operator and " << str << endl;
+//		}
+
+		void operator&&(string str) {
+			cout << "Yannis: operator && " << str << endl;
 		}
 };
+
+Yannis y;
+
+bool first() {
+	y << "first";
+	return false;
+}
+
+bool second() {
+	y << "second";
+	return true;
+}
 
 int main() {
 	string var = "I'm a var!";
 
-	Yannis y;
-	y << "Hello World!!";
+	y << "Hello World!!" << "toto";
 	y << (string) var;
 	NL;
 	y << yannis;
 	NL;
+
+	y and "coucou";
+	y && "coucou";
+	if (first() and second()) {
+		y << "true";
+	}
+
+	if (first() && second()) {
+		y << "true";
+	}
+
+	int a = 1;
+	a &= 2;
+	y << "a=" << a;
+	a and_eq 2;
+	y << "a=" << a;
+	a = 1;
+	a and_eq 3;
+	y << "a=" << a;
+
+	int foo = 10, bar = 15;
+	int res = 0;
+    asm volatile("addl  %%ebx,%%eax"
+                         :"=a"(res)
+                         :"a"(foo), "b"(bar)
+                         );
+    y << "foo+bar=" << res;
+    y << "foo=" << foo;
+    y << "bar=" << bar;
+    return 0;
 }
