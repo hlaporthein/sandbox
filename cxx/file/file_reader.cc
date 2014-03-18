04bbea0e2f535b48d82cfd7f15b7a33cd3b15365
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-03-18 15:11:29 +0100

    opti

--- cxx/file/file_reader.cc
+++ cxx/file/file_reader.cc
@@ -16,28 +16,36 @@ void print_hexa(const string file_name) {
 		is.open(file_name, ifstream::in | ifstream::binary);
 		is.exceptions(ifstream::badbit);
 
-		unsigned char buffer[17];
-		memset(buffer, 0, 17);
+		const int buffer_size = 1 << 16;
+
+		char buffer[buffer_size];
+		memset(buffer, 0, buffer_size);
 		int s = 0;
 		int address = 0;
 
 		while (!is.eof()) {
-			is.read((char*) buffer, 16);
+			is.read(buffer, buffer_size);
 			s = is.gcount();
 
-			char buffer_hexa[64] = "";
-			for (int i = 0; i < s; i++) {
-				char space[2] = "";
-				if (i % 4 == 0) {
-					snprintf(space, 2, " ");
+			for (int i = 0; i < s; i += 16) {
+				char buffer_hexa[64] = "";
+				int c = s - i;
+				if (c >= 16) {
+					c = 16;
 				}
-				char str[32];
-				int x = (int) buffer[i];
-				snprintf(str, 32, " %s%02x", space, x);
-				strcat(buffer_hexa, str);
+				for (int j = 0; j < c; j++) {
+					char space[2] = "";
+					if (j % 4 == 0) {
+						snprintf(space, 2, " ");
+					}
+					char str[32];
+					int x = (int) (unsigned char) buffer[i + j];
+					snprintf(str, 32, " %s%02x", space, x);
+					strcat(buffer_hexa, str);
+				}
+				printf("%08x:%s\n", address, buffer_hexa);
+				address += 16;
 			}
-			printf("%08x:%s\n", address, buffer_hexa);
-			address += 16;
 		}
 
 		is.close();
