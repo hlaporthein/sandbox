1e9a4a488aeed174e5e31a3c318c89088ce39f0b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2014-03-31 00:22:13 +0200

    oo

---
+++ exception_test/exception_test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <windows.h>
+//typedef struct _EXCEPTION_RECORD {
+//	DWORD ExceptionCode;
+//	DWORD ExceptionFlags;
+//	struct _EXCEPTION_RECORD *ExceptionRecord;
+//	PVOID ExceptionAddress;
+//	DWORD NumberParameters;
+//	DWORD ExceptionInformation[EXCEPTION_MAXIMUM_PARAMETERS];
+//} EXCEPTION_RECORD,*PEXCEPTION_RECORD,*LPEXCEPTION_RECORD;
+//typedef struct _EXCEPTION_POINTERS {
+//	PEXCEPTION_RECORD ExceptionRecord;
+//	PCONTEXT ContextRecord;
+//} EXCEPTION_POINTERS,*PEXCEPTION_POINTERS,*LPEXCEPTION_POINTERS;
+LPVOID DisplayError(DWORD Err)
+{
+
+	HMODULE Hand = LoadLibrary("NTDLL.DLL");
+	LPVOID lpMessageBuffer;
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_FROM_HMODULE,
+		Hand,
+		Err,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMessageBuffer,
+		0,
+		NULL );
+
+	FreeLibrary(Hand);
+	return lpMessageBuffer;
+}
+
+LONG WINAPI e(LPEXCEPTION_POINTERS ExceptionInfo) {
+	printf("Exception Handled...\n");
+	char *buf = DisplayError(ExceptionInfo->ExceptionRecord->ExceptionCode);
+	printf("ExceptionCode=0x%08x (%s)\n", ExceptionInfo->ExceptionRecord->ExceptionCode, buf);
+	printf("ExceptionFlags=%d\n", ExceptionInfo->ExceptionRecord->ExceptionFlags);
+	printf("ExceptionAddress=0x%08x\n", ExceptionInfo->ExceptionRecord->ExceptionAddress);
+	printf("ExceptionInformation=%s\n", ExceptionInfo->ExceptionRecord->ExceptionInformation);
+
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+int main() {
+	LPTOP_LEVEL_EXCEPTION_FILTER p = SetUnhandledExceptionFilter(e);
+	if (p == NULL) {
+		printf("Exception Handling Default behavior.\n");
+	} else {
+		printf("Exception Handling Function = 0x%08x.\n");
+	}
+
+	int i = 0;
+	int j = 0;
+	int k = i / j;
+	printf("i/j = %d\n", k);
+	return 0;
+}
