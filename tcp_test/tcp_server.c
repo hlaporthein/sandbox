7e1da791c2f9dc9222e44a4e3052f7a4e3129001
Jean-Louis GUENEGO <jlguenego@gmail.com>
2014-07-12 19:00:22 +0200

    tcp test portable msys & mingw32

--- tcp_test/tcp_server.c
+++ tcp_test/tcp_server.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h> // for write and closing socket
+#include <errno.h>
 
 #define PORT 22222
 #define LISTEN_QUEUE_SIZE 5
@@ -20,18 +21,17 @@
 
 #define BUFFER_SIZE 1024
 
-static int s_return_code = 0;
-
-#define CHECK_ERROR(condition, ...) \
+#define TRY(statement, condition, ...) \
+	statement; \
 	if (condition) { \
-		char my_buffer[BUFFER_SIZE] = ""; \
-		snprintf(my_buffer, BUFFER_SIZE, __VA_ARGS__); \
-		fprintf(stderr, my_buffer); \
-		s_return_code = 1; \
+		fprintf(stderr, __VA_ARGS__); \
+		result = 1; \
+		errno = 0; \
 		goto cleanup; \
 	} \
 
 int handle_connection(int socket) {
+	int result = 0;
 	// here a socket is like a stream referenced by an integer
 	// print on the screen the stream...
 
@@ -39,7 +39,9 @@ int handle_connection(int socket) {
 		int size = 16;
 		int qty_read = 0;
 		char buffer[16] = "";
-		CHECK_ERROR((qty_read = read(socket, buffer, size - 1)) < 0, "Error while reading the socket content.\n");
+		qty_read = TRY(recv(socket, buffer, size - 1, 0),
+			qty_read < 0,
+			"Error while reading the socket content.\n");
 		buffer[15] = '\0';
 		printf("Socket content: %s\n", buffer);
 		if (qty_read != size - 1) {
@@ -50,11 +52,10 @@ int handle_connection(int socket) {
 
 	// write on the socket file descriptor an answer.
 	char *answer = "Message received!";
-	int ret = write(socket, answer, strlen(answer));
-	CHECK_ERROR(ret < 0, "Error while answering");
-
+	int ret = TRY(send(socket, answer, strlen(answer) + 1, 0), ret < 0, "Error while answering");
+	printf("ret=%d\n", ret);
 cleanup:
-	return s_return_code;
+	return result;
 }
 
 int main() {
@@ -68,44 +69,53 @@ int main() {
 #endif
 	printf("Starting TCP Server on Port %d.\n", PORT);
 
+
+	int result = 0;
 	// start the server
 	// create a socket pointer
 	// int socket(int domain, int type, int protocol);
 	// domain : AF_UNIX (File system pathnames.) or AF_INET (Internet address.)
 	// type : SOCK_STREAM, etc. (always SOCK_STREAM for a bidirectional socket (to be investigated later)
 	int server_socket = 0;
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	// test for error
-	// Upon successful completion, socket() returns a nonnegative integer, the socket file descriptor. Otherwise a value of -1 is returned and errno is set to indicate the error.
-	CHECK_ERROR(server_socket < 0, "ECHOSERV: Error creating listening socket on port %d.\n", PORT);
+	server_socket = TRY(socket(AF_INET, SOCK_STREAM, 0),
+		server_socket < 0,
+		"ERROR opening socket (%d: %s)", errno, strerror(errno));
 
 	struct    sockaddr_in servaddr;
 	// set all the fields to 0
 	memset(&servaddr, 0, sizeof(servaddr));
 	// set some fields to the desired values
-    servaddr.sin_family      = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port        = htons(PORT);
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(PORT);
 
-	CHECK_ERROR(bind(server_socket, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0, "ECHOSERV: Error calling bind()\n");
+	result = TRY(bind(server_socket, (struct sockaddr *) &servaddr, sizeof(servaddr)), result < 0, "ECHOSERV: Error calling bind()\n");
 
-	CHECK_ERROR(listen(server_socket, LISTEN_QUEUE_SIZE) < 0, "ECHOSERV: Error calling listen()\n");
+	result = TRY(listen(server_socket, LISTEN_QUEUE_SIZE), result < 0, "ECHOSERV: Error calling listen()\n");
 
 	while (TRUE) {
 
 		// Wait for a connection, then accept() it
 		int client_socket = 0;
-		CHECK_ERROR((client_socket = accept(server_socket, NULL, NULL)) < 0, "ECHOSERV: Error calling accept()\n");
+		TRY((client_socket = accept(server_socket, NULL, NULL)),
+			client_socket < 0,
+			"ECHOSERV: Error calling accept()\n");
 
-		CHECK_ERROR(handle_connection(client_socket), "Error while handling a connection\n");
+		result = TRY(handle_connection(client_socket),
+			result != 0,
+			"Error while handling a connection\n");
 
 		// Close the connected socket
-		CHECK_ERROR(close(client_socket) < 0, "ECHOSERV: Error calling close()\n");
+#ifdef __MINGW32__
+#define close closesocket
+#endif
+		result = TRY(close(client_socket),
+			result < 0, "ECHOSERV: Error calling close(): %d %s\n", errno, strerror(errno));
 
 	}
 
 
 cleanup:
-	return s_return_code;
+	return result;
 }
 
