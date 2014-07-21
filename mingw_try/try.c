5eaf7d8b0ff62059e0e7359bac2ac826c22fb6c1
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-07-21 17:05:59 +0200

    ok

--- mingw_try/try.c
+++ mingw_try/try.c
@@ -5,7 +5,7 @@ EXCEPTION_DISPOSITION coucou(struct _EXCEPTION_RECORD *er, void *v, struct _CONT
 	printf("Hello we got an exception. Exception code=0x%08X\n", er->ExceptionCode);
 	int ebx;
 	__asm__ __volatile__ ("movl %0, %%ebx; movl $1, 8(%%ebx)" : "=r" (ebx) : "r" (context->Ebp));
-	
+
 	printf("ebx=0x%08X\n", ebx);
 	return ExceptionContinueExecution;
 }
@@ -17,10 +17,9 @@ void oops(int b) {
 }
 
 int main() {
-	PEXCEPTION_HANDLER pCoucou = coucou;
-	__try1(pCoucou) {
+	__try1(&coucou) {
 		oops(0);
-		
+
 	} __except1;
 	oops(2);
 
