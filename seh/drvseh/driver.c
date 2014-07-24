1940d116e0a1d85f155b9fd40f466fd7ad820b16
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-07-24 16:29:36 +0200

    static

--- seh/drvseh/driver.c
+++ seh/drvseh/driver.c
@@ -2,6 +2,7 @@
 #include "driver.h"
 
 PDRIVER_OBJECT p = NULL;
+char *str = "Hello";
 
 NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING registryPath) {
 	NTSTATUS status = STATUS_SUCCESS;
@@ -9,6 +10,7 @@ NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regist
 
 
 	DbgPrint("Hello!!\n");
+	DbgPrint("str=%s\n", str);
 	__drv_try {
 		DbgPrint("Success\n");
 	} __drv_except {
@@ -19,14 +21,14 @@ NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regist
 
 	p->DriverUnload = my_unload;
 
-	__drv_try {
-		DbgPrint("About to probe\n");
-		ProbeForWrite((PVOID) 0, 10, 4);
-		DbgPrint("Everything ok\n");
-	} __drv_except {
-		DbgPrint("Exception catched.\n");
-	} __drv_end_except;
-	DbgPrint("RegistryPath=%wZ\n", registryPath);
+//	__drv_try {
+//		DbgPrint("About to probe\n");
+//		ProbeForWrite((PVOID) 0, 10, 4);
+//		DbgPrint("Everything ok\n");
+//	} __drv_except {
+//		DbgPrint("Exception catched.\n");
+//	} __drv_end_except;
+//	DbgPrint("RegistryPath=%wZ\n", registryPath);
 
 cleanup:
 	return status;
@@ -34,4 +36,5 @@ cleanup:
 
 VOID STDCALL my_unload(PDRIVER_OBJECT DriverObject) {
 	DbgPrint("GoodBye!!\n");
+	DbgPrint("str=%s\n", str);
 }
