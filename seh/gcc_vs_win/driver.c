e3407db4ca2841ed9b714e2f6863e56e6303e3cf
THOMIAS Yannis <thomias.yannis@gmail.com>
2014-07-25 13:14:47 +0200

    ok

--- seh/gcc_vs_win/driver.c
+++ seh/gcc_vs_win/driver.c
@@ -1,10 +1,15 @@
 #include <ntddk.h>
 
-void DriverUnload(PDRIVER_OBJECT pDriverObject) {
+#ifdef __MINGW32__
+#else
+#define STDCALL
+#endif // _MINGW
+
+void STDCALL DriverUnload(PDRIVER_OBJECT pDriverObject) {
 	DbgPrint("Driver unloading\n");
 }
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+NTSTATUS STDCALL DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 	DriverObject->DriverUnload = DriverUnload;
 	DbgPrint("Hello, World\n");
 	return STATUS_SUCCESS;
