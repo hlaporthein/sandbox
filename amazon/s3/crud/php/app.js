1f1c0ee2ca5de684b497639f9e079e7e641b2592
Jean-Louis GUENEGO <jlguenego@gmail.com>
2015-07-30 16:44:03 +0200

    ok

--- amazon/s3/crud/php/app.js
+++ amazon/s3/crud/php/app.js
@@ -74,13 +74,14 @@
                 var file = files[i];
                 Upload.upload({
                     url: 'myAmazon.php',
-                    fields: {'upload': undefined},
+                    fields: {'upload': ''},
                     file: file
                 }).progress(function (evt) {
                     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                     console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
                 }).success(function (data, status, headers, config) {
-                    console.log('file ' + config.file.name + 'uploaded. Response: ', data);
+                	console.log('config', config);
+                    console.log('file ' + config.file.name + ' uploaded. Response: ', data);
                 }).error(function (data, status, headers, config) {
                     console.log('error status: ' + status);
                 }).finally(function() {
